71b0b760cd5a0331b05feb5fb3491c2bccc5ed7c
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-01-12 16:47:23 -0300

    Revert "teste2"
    This reverts commit 2c2587fc2320918b1c1dc642bb9bd451554bbe09.

--- lib/helpers/guards/login_guard.dart
+++ lib/helpers/guards/login_guard.dart
@@ -4,8 +4,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intelicity_auth_microapp_flutter/core/auth_controller.dart';
 
 class LoginGuard extends RouteGuard {
-  LoginGuard({String redirectTo = '/login/logged'})
-      : super(redirectTo: redirectTo);
+  LoginGuard({String redirectTo = '/'}) : super(redirectTo: redirectTo);
   @override
   FutureOr<bool> canActivate(String path, ParallelRoute route) async {
     return !Modular.get<AuthController>().isLogged;
