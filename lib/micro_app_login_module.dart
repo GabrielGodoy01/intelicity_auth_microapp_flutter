26eb6dce827af38ad3d72dc9b1822fd64fe7d544
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-01-12 16:26:28 -0300

    Update micro_app_login_module.dart

--- lib/micro_app_login_module.dart
+++ lib/micro_app_login_module.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intelicity_auth_microapp_flutter/amplify/amplifyconfiguration.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/enum/role_enum.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/confirm_new_password_usecase.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/confirm_reset_password.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/login_with_email_usecase.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/reset_password_usecase.dart';
+import 'package:intelicity_auth_microapp_flutter/micro_app_auth_module.dart';
 import 'package:intelicity_auth_microapp_flutter/presenter/controllers/confirm_reset_password_controller.dart';
 import 'package:intelicity_auth_microapp_flutter/presenter/controllers/forgot_password_controller.dart';
 import 'package:intelicity_auth_microapp_flutter/presenter/controllers/login_controller.dart';
@@ -15,6 +17,9 @@ import 'package:intelicity_auth_microapp_flutter/presenter/ui/pages/new_password
 import 'package:intelicity_auth_microapp_flutter/presenter/ui/pages/success_login_page.dart';
 
 class MicroAppLoginModule extends Module {
+  @override
+  List<Module> get imports => [MicroAppAuthModule(amplifyconfig)];
+
   @override
   void binds(i) {
     i.add<LoginController>(LoginController.new);
