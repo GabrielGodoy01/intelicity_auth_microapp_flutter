2a7fd30138a5f5fc401a763d5a226352855ee4cd
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-02-06 19:17:30 -0300

    Update micro_app_auth_module.dart

--- lib/micro_app_auth_module.dart
+++ lib/micro_app_auth_module.dart
@@ -27,7 +27,7 @@ class MicroAppAuthModule extends Module {
     i.addLazySingleton<IHttpRequest>((i) => DioHttpRequest(dio: i<Dio>()));
     i.addLazySingleton(AuthController.new);
     i.addLazySingleton(Logger.new);
-    i.add<IAuthDatasource>(CognitoDatasource.new);
+    i.add<IAuthDatasource>((i) => CognitoDatasource(i()));
     i.addLazySingleton<IAuthRepository>(AuthRepositoryCognito.new);
     i.addLazySingleton<IGetLoggedUserUsecase>(GetLoggedUserImpl.new);
     i.addLazySingleton<ILogoutUsecase>(LogoutUsecaseImpl.new);
