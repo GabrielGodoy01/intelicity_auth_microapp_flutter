2bd30f77721f7e58a0d32d8eb97cbb454ed8507b
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-02-26 15:35:47 -0300

    Update management_group_page.dart

--- lib/presenter/ui/pages/admin/management_group_page.dart
+++ lib/presenter/ui/pages/admin/management_group_page.dart
@@ -43,43 +43,41 @@ class _ManagementGroupPageState extends State<ManagementGroupPage> {
                 ? LayoutBuilder(builder: (context, constraints) {
                     var obraTableDataSource = ObraDataSourceTable(
                         users: state.users, context: context);
-                    return SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: ScreenHelper.height(context) * 0.8,
-                            child: SfDataGrid(
-                              controller: _dataGridController,
-                              allowSorting: true,
-                              allowMultiColumnSorting: true,
-                              columnWidthMode: ColumnWidthMode.lastColumnFill,
-                              rowHeight: 80,
-                              selectionMode: SelectionMode.multiple,
-                              source: obraTableDataSource,
-                              columns: [
-                                _getColumn('sub', 'ID'),
-                                _getColumn('role', 'Função'),
-                                _getColumn('email', 'E-mail'),
-                                _getColumn('name', 'Nome'),
-                                _getColumn('edit', null),
-                              ],
-                            ),
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: ScreenHelper.height(context) * 0.8,
+                          child: SfDataGrid(
+                            controller: _dataGridController,
+                            allowSorting: true,
+                            allowMultiColumnSorting: true,
+                            columnWidthMode: ColumnWidthMode.lastColumnFill,
+                            rowHeight: 80,
+                            selectionMode: SelectionMode.multiple,
+                            source: obraTableDataSource,
+                            columns: [
+                              _getColumn('sub', 'ID'),
+                              _getColumn('role', 'Função'),
+                              _getColumn('email', 'E-mail'),
+                              _getColumn('name', 'Nome'),
+                              _getColumn('edit', null),
+                            ],
                           ),
-                          SizedBox(
-                              height: _dataPagerHeight,
-                              child: SfDataPagerTheme(
-                                data: SfDataPagerThemeData(
-                                  selectedItemColor: AppColors.primaryPurple,
-                                  backgroundColor: Colors.white,
-                                ),
-                                child: SfDataPager(
-                                  delegate: obraTableDataSource,
-                                  pageCount: state.users.length / _rowsPerPage,
-                                  direction: Axis.horizontal,
-                                ),
-                              ))
-                        ],
-                      ),
+                        ),
+                        SizedBox(
+                            height: _dataPagerHeight,
+                            child: SfDataPagerTheme(
+                              data: SfDataPagerThemeData(
+                                selectedItemColor: AppColors.primaryPurple,
+                                backgroundColor: Colors.white,
+                              ),
+                              child: SfDataPager(
+                                delegate: obraTableDataSource,
+                                pageCount: state.users.length / _rowsPerPage,
+                                direction: Axis.horizontal,
+                              ),
+                            ))
+                      ],
                     );
                   })
                 : const Center(child: CircularProgressIndicator());
@@ -89,6 +87,7 @@ class _ManagementGroupPageState extends State<ManagementGroupPage> {
 
   GridColumn _getColumn(String columnName, String? label) {
     return GridColumn(
+      maximumWidth: ScreenHelper.width(context) * 0.15,
       columnName: columnName,
       label: Container(
         padding: const EdgeInsets.all(16.0),
