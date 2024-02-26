4ced63d8610bfa86640d18d46cce725236303b97
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-02-26 16:27:01 -0300

    Update micro_app_admin_module.dart

--- lib/micro_app_admin_module.dart
+++ lib/micro_app_admin_module.dart
@@ -2,10 +2,12 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intelicity_auth_microapp_flutter/amplify/amplifyconfiguration.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/admin_create_user_usecase.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/usecases/list_users_in_group_usecase.dart';
+import 'package:intelicity_auth_microapp_flutter/domain/usecases/update_user_usecase.dart';
 import 'package:intelicity_auth_microapp_flutter/helpers/guards/admin_guard.dart';
 import 'package:intelicity_auth_microapp_flutter/micro_app_auth_module.dart';
 import 'package:intelicity_auth_microapp_flutter/micro_app_users_management_module.dart';
-import 'package:intelicity_auth_microapp_flutter/presenter/controllers/create_user_controller.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/controllers/admin/create_user_controller.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/controllers/admin/update_user_controller.dart';
 import 'package:intelicity_auth_microapp_flutter/presenter/ui/pages/admin/create_user_page.dart';
 import 'package:intelicity_auth_microapp_flutter/presenter/ui/pages/admin/home_admin_page.dart';
 
@@ -17,6 +19,8 @@ class MicroAppAdminModule extends Module {
   void binds(i) {
     i.addLazySingleton<IAdminCreateUserUsecase>(AdminCreateUserUsecaseImpl.new);
     i.addLazySingleton<CreateUserController>(CreateUserController.new);
+    i.addLazySingleton<IUpdateUserUsecase>(UpdateUserUsecaseImpl.new);
+    i.addLazySingleton<UpdateUserController>(UpdateUserController.new);
 
     i.addLazySingleton<IListUsersInGroupUsecase>(
         ListUsersInGroupUsecaseImpl.new);
