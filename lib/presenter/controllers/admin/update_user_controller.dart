89927f73f50f3b486053da8d9cd25df50dd01de5
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-02-26 16:19:13 -0300

    UPDATE DIALOG

---
+++ lib/presenter/controllers/admin/update_user_controller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intelicity_auth_microapp_flutter/core/auth_controller.dart';
+import 'package:intelicity_auth_microapp_flutter/domain/enum/role_enum.dart';
+import 'package:intelicity_auth_microapp_flutter/domain/usecases/update_user_usecase.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/states/basic_state.dart';
+import 'package:logger/logger.dart';
+import 'package:mobx/mobx.dart';
+
+part 'update_user_controller.g.dart';
+
+class UpdateUserController = UpdateUserControllerBase
+    with _$UpdateUserController;
+
+abstract class UpdateUserControllerBase with Store {
+  final Logger logger = Modular.get();
+  var authController = Modular.get<AuthController>();
+
+  final IUpdateUserUsecase _updateUser;
+
+  UpdateUserControllerBase(this._updateUser);
+
+  @observable
+  BasicState state = BasicInitialState();
+
+  @action
+  void setState(BasicState value) => state = value;
+
+  Future<void> updateUser(
+    String email,
+    String name,
+    RoleEnum role,
+    List<String> selectedGroups,
+  ) async {
+    setState(BasicLoadingState());
+    final result = await _updateUser(
+      email: email,
+      name: name,
+      role: role,
+      groups: selectedGroups,
+    );
+    setState(result.fold((e) {
+      logger.e(e.message);
+      return BasicErrorState(error: e);
+    }, (_) {
+      return BasicInitialState();
+    }));
+  }
+}
--- lib/presenter/ui/dialogs/update_user_dialog.dart
+++ lib/presenter/ui/dialogs/update_user_dialog.dart
@@ -1,12 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intelicity_auth_microapp_flutter/core/auth_controller.dart';
 import 'package:intelicity_auth_microapp_flutter/domain/entities/user_info.dart';
+import 'package:intelicity_auth_microapp_flutter/domain/enum/group_enum.dart';
+import 'package:intelicity_auth_microapp_flutter/domain/enum/role_enum.dart';
+import 'package:intelicity_auth_microapp_flutter/generated/l10n.dart';
+import 'package:intelicity_auth_microapp_flutter/helpers/utils/validation_field.dart';
+import 'package:intelicity_auth_microapp_flutter/infra/models/group_model.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/controllers/admin/update_user_controller.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/states/basic_state.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/ui/widgets/button_custom.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/ui/widgets/drop_down_field_custom.dart';
+import 'package:intelicity_auth_microapp_flutter/presenter/ui/widgets/text_field_custom.dart';
+import 'package:intelicity_auth_microapp_flutter/shared/themes/app_colors.dart';
+import 'package:intelicity_auth_microapp_flutter/shared/themes/app_text_styles.dart';
 
-class UpdateUserDialog extends StatelessWidget {
+class UpdateUserDialog extends StatefulWidget {
   final UserInfo user;
   const UpdateUserDialog({super.key, required this.user});
 
   @override
+  State<UpdateUserDialog> createState() => _UpdateUserDialogState();
+}
+
+class _UpdateUserDialogState extends State<UpdateUserDialog> {
+  final AuthController authController = Modular.get();
+  final UpdateUserController controller = Modular.get();
+  final nameController = TextEditingController();
+  var groups = <GroupModel>[];
+  RoleEnum? role;
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    for (GroupEnum item in authController.user!.groups) {
+      groups.add(GroupModel(groupName: item, isSelected: false));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Dialog();
+    return Dialog(
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Atualizar usuário: ${widget.user.email}',
+              style: AppTextStyles.headline1,
+            ),
+            const SizedBox(height: 8),
+            TextFielCustom(
+              hintText: 'Nome',
+              controller: nameController,
+              prefixIcon: Icons.person,
+              validation: ValidationFieldHelper.validateRequiredField,
+            ),
+            const SizedBox(height: 8),
+            DropDownFieldWidget<RoleEnum>(
+              hintText: S.of(context).role,
+              prefixIcon: Icons.work,
+              onChanged: (value) {
+                role = value;
+              },
+              validation: ValidationFieldHelper.validateRole,
+              items: RoleEnum.values.map((RoleEnum value) {
+                return DropdownMenuItem<RoleEnum>(
+                  value: value,
+                  child: Text(value.typeName),
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Permissão de Sistemas:',
+                style: AppTextStyles.bodyText1,
+                textAlign: TextAlign.start,
+              ),
+            ),
+            const SizedBox(height: 8),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: groups.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(groups[index].groupName.name),
+                  value: groups[index].isSelected,
+                  activeColor: AppColors.primaryPurple,
+                  controlAffinity: ListTileControlAffinity.leading,
+                  onChanged: (value) {
+                    setState(() {
+                      groups[index].isSelected = value!;
+                    });
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            ButtonCustom(
+              text: S.of(context).register,
+              isLoading: controller.state is BasicLoadingState,
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  await controller.updateUser(
+                    widget.user.email,
+                    nameController.text,
+                    role!,
+                    groups
+                        .where((element) => element.isSelected)
+                        .map((e) => e.groupName.name)
+                        .toList(),
+                  );
+                }
+                if (controller.state is BasicInitialState) {
+                  setState(() {
+                    formKey.currentState!.reset();
+                    nameController.clear();
+                    role = null;
+                    groups = [];
+                    for (GroupEnum item in authController.user!.groups) {
+                      groups
+                          .add(GroupModel(groupName: item, isSelected: false));
+                    }
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
